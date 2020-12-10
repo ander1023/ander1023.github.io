b35ab0c3a15301e1af33d3c051412f8cc172b2ba
ander1023 <1045277340@qq.com>
2020-12-11 01:22:12 +0800

    添加初始网页

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function getScorll(){
+    var scrollTop=0;
+    if(document.documentElement&&document.documentElement.scrollTop){
+        scrollTop=document.documentElement.scrollTop;
+    }else if(document.body){
+        scrollTop=document.body.scrollTop;
+    }
+    console.log(scrollTop);
+}
